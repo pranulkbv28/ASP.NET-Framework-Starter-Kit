aef455121932d2adc5fb6f8d4fad2f892be3965b
pranulkbv28 <28bvpranaav@gmail.com>
2025-02-05 19:19:18 +0530

    Add configuration loading and JavaScript interop to ApiService

--- Blazor for Front-End Development/!Test App/MyBlazorApp/Services/ApiService.cs
+++ Blazor for Front-End Development/!Test App/MyBlazorApp/Services/ApiService.cs
@@ -1,18 +1,22 @@
 using System;
+using Microsoft.JSInterop;
 
 namespace MyBlazorApp.Services;
 
 public class ApiService
 {
     private readonly IConfiguration? _configuration;
+    private readonly IJSRuntime _jsRuntime;
 
-    public ApiService(IConfiguration configuration)
+    public ApiService(IConfiguration configuration, IJSRuntime jsRuntime)
     {
         _configuration = configuration;
+        _jsRuntime = jsRuntime;
     }
 
     public string GetApiUrl()
     {
+        _jsRuntime.InvokeVoidAsync("console.log", _configuration?["ApiSettings:BaseUrl"]);
         return _configuration?["ApiSettings:BaseUrl"] ?? "https://localhost:5001";
     }
 }
